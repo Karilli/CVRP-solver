003f5e08af31395afa2f3d24a4ce073a4835427c
Karilli <jos3f.karas@gmail.com>
2024-07-13 20:56:52 +0200

    timing with garbage collection

--- other/time_TSP.py
+++ other/time_TSP.py
@@ -9,33 +9,34 @@ from test_TSP import random_problem
 from time import perf_counter
 from math import inf
 import random
+import gc
 
 
-N = 10
-MAX = 15
+N = 5
+MAX = 20
 
 
 def main():
     locations, dist = random_problem(100)
     Memo.resize(MAX)
 
-    for i in range(1, MAX):
+    for i in range(1, MAX+1):
         res = {}
         for f in dp, dijkstra, brute_force:
             total = 0
-            if 10 < i and f is brute_force:
+            if 10 <= i and f is brute_force:
                 res[f] = inf
                 continue
             for _ in range(N):      
                 route = random.sample(list(range(1, len(locations))), k=i)
+                gc.collect()
                 t = perf_counter()
                 f(route, dist)
                 total += perf_counter() - t
             res[f] = total / N
 
-        t, _, f = min((res[f], j, str(f).split()[1]) for j, f in enumerate((dp, dijkstra, brute_force)))
-        x = "'"
-        print(f"{i}: ({t}, {f.replace(x, '')})")
+        t, _, f = min((res[f], j, str(f).split()[1].replace("'", "")) for j, f in enumerate((dp, dijkstra, brute_force)))
+        print(f"{i}: ({t}, {f})")
 
 
 if __name__ == "__main__":
